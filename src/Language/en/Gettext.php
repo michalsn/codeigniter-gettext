a8a21c419e1c3d81070205a6b6dc51cd1565dfac
michalsn <michal@sniatala.pl>
2025-10-15 13:35:56 +0200

    workflow update

--- src/Language/en/Gettext.php
+++ src/Language/en/Gettext.php
@@ -4,5 +4,4 @@ return [
     'localeNotSupported' => 'Locale is not supported',
     'dirDoesNotExist'    => 'Dir for gettext does not exist.',
     'domainNotSupported' => 'The domain is not supported.',
-
 ];
